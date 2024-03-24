296d2f1d71330c65750df8e2bcbb7f20534b7e77
MaNaSDeo <rmnslogin@gmail.com>
2024-03-24 15:44:25 +0530

    saveUser

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import axios from "axios";
+import User from "../models/Users";
 
 const save = (req: Request, res: Response): void => {
   console.log("Inside save function in controller");
@@ -7,7 +9,28 @@ const save = (req: Request, res: Response): void => {
 
 const saveUser = async (req: Request, res: Response) => {
   const { username } = req.params;
-  res.json(username);
+  try {
+    const existingUser = await User.findOne({
+      username: new RegExp(`^${username}$`, "i"),
+    }); // Check if the user already exists in the database
+
+    if (existingUser) {
+      return res
+        .status(200)
+        .json({ message: "User already exists", user: existingUser });
+    }
+
+    const response = await axios.get(`${process.env.GIT_URL}/${username}`); //Fetch data from Github API
+    const userData = response.data;
+
+    const newUser = new User({ ...userData, username: userData.login }); // Create a new user document
+
+    await newUser.save(); // Save the user document to the database
+
+    res.status(201).json({ message: "User saved successfully", user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: `Error fetching or saving user`, error });
+  }
 };
 
 const findMutualFollowers = async (req: Request, res: Response) => {
